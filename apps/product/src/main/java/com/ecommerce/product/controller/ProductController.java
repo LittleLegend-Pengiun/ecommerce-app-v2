3bfb18a7f19c104709c7702eeaf61e2bf2f6faa0
hdthinh1012 <hdthinh01@gmail.com>
2024-12-14 14:41:56 +0700

    Responsity status code for Product API

--- apps/product/src/main/java/com/ecommerce/product/controller/ProductController.java
+++ apps/product/src/main/java/com/ecommerce/product/controller/ProductController.java
@@ -3,6 +3,7 @@ import com.ecommerce.product.dto.ProductDto;
 import com.ecommerce.product.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -18,22 +19,22 @@ public class ProductController {
 
     @GetMapping("/all")
     public ResponseEntity<List<ProductDto>> getAllProducts() {
-        return ResponseEntity.ok().body(productService.getAllProducts());
+        return ResponseEntity.status(HttpStatus.OK).body(productService.getAllProducts());
     }
 
     @PostMapping("")
     public ResponseEntity<ProductDto> saveProduct(@Valid @RequestBody ProductDto dto){
-        return ResponseEntity.ok().body(productService.saveProduct(dto));
+        return ResponseEntity.status(HttpStatus.OK).body(productService.saveProduct(dto));
     }
 
     @PutMapping("")
     public ResponseEntity<ProductDto> updateProduct(@Valid @RequestBody ProductDto dto){
-        return ResponseEntity.ok().body(productService.updateProduct(dto));
+        return ResponseEntity.status(HttpStatus.OK).body(productService.updateProduct(dto));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProductById(@Valid @PathVariable Integer id) {
         productService.deleteProductById(id);
-        return ResponseEntity.ok().body("Delete product successfully!");
+        return ResponseEntity.status(HttpStatus.OK).body("Delete product successfully!");
     }
 }
