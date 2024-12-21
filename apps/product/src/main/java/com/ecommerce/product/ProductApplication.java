49ac47d97e93f3fdc1d5f06a093c74c5d1fdccda
Hoang Anh <hoanganhleboy@gmail.com>
2024-12-21 14:07:40 +0700

    Clean up

--- apps/product/src/main/java/com/ecommerce/product/ProductApplication.java
+++ apps/product/src/main/java/com/ecommerce/product/ProductApplication.java
@@ -1,14 +1,9 @@
 package com.ecommerce.product;
 
 import com.ecommerce.product.service.DataInitializer;
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.io.IOException;
 
