9cce7e07d2e1ef400d20d82fe1aa539c6b615e7d
hdthinh1012 <hdthinh01@gmail.com>
2025-01-19 17:40:31 +0700

    Init

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -7,33 +7,41 @@ import { productsAtom } from "@/state/atoms/products";
 
 const { Title } = Typography;
 
-export default function Home() {
+// export default function Home() {
 
-  const [products] = useAtom(productsAtom);
-  const [cart, setCart] = useAtom(cartAtom);
+//   const [products] = useAtom(productsAtom);
+//   const [cart, setCart] = useAtom(cartAtom);
 
-  const addToCart = (product: Product) => {
-    setCart((prevCart: Cart): Cart => {
-      const existingItem = prevCart.find((item) => item.id === product.id);
-      if (existingItem) {
-        return prevCart.map((item) => (item.id === product.id) ? { ...item, cartQuantity: item.cartQuantity + 1 } : item);
-      }
-      return [...prevCart, { ...product, cartQuantity: 1 }];
-    });
-  };
+//   const addToCart = (product: Product) => {
+//     setCart((prevCart: Cart): Cart => {
+//       const existingItem = prevCart.find((item) => item.id === product.id);
+//       if (existingItem) {
+//         return prevCart.map((item) => (item.id === product.id) ? { ...item, cartQuantity: item.cartQuantity + 1 } : item);
+//       }
+//       return [...prevCart, { ...product, cartQuantity: 1 }];
+//     });
+//   };
 
+//   return (
+//     <>
+//       <Title level={1}>Home</Title>
+
+//       <ul>
+//         {products.map((product) => (
+//           <li key={product.id} style={{ marginBottom: "1em" }}>
+//             <span style={{ marginRight: "1em" }}>{product.productName}</span>
+//             <button onClick={() => addToCart(product)}>Add to Cart</button>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+function HomePage() {
   return (
-    <>
-      <Title level={1}>Home</Title>
-
-      <ul>
-        {products.map((product) => (
-          <li key={product.id} style={{ marginBottom: "1em" }}>
-            <span style={{ marginRight: "1em" }}>{product.productName}</span>
-            <button onClick={() => addToCart(product)}>Add to Cart</button>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+    <Title level={1}>Home</Title>
+  )
 }
+
+export default HomePage;
