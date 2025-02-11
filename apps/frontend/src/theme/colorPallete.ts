cf34aaa62ed22cbf3f20a38138313dc6d522aec2
Hoang Anh <hoanganhleboy@gmail.com>
2025-02-11 23:12:35 +0700

    Add color pallete

---
+++ apps/frontend/src/theme/colorPallete.ts
@@ -0,0 +1,11 @@
+export const colorPallete = {
+    primary1: '#ffffff',
+    text1: '#D37643',
+    text2: '#D37643',
+    secondary1: '#FEFAF1',
+    secondary2: '#DB4444',
+    button1: '#47B486',
+    button2: '#DB4444',
+    hoverButton1: '#E2B93B',
+    hoverButton2: '#EB5757'
+}
