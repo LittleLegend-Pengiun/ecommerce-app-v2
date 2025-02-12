8b950e1333703a8fa1edeae2b008abda9d0b50ee
Hoang Anh <hoanganhleboy@gmail.com>
2025-02-12 20:03:52 +0700

    Update color

--- apps/frontend/src/theme/colorPallete.ts
+++ apps/frontend/src/theme/colorPallete.ts
@@ -1,11 +1,12 @@
 export const colorPallete = {
     primary1: '#ffffff',
-    text1: '#D37643',
-    text2: '#D37643',
+    text1: '#000000',
+    text2: '#7D8184',
     secondary1: '#FEFAF1',
     secondary2: '#DB4444',
     button1: '#47B486',
     button2: '#DB4444',
     hoverButton1: '#E2B93B',
-    hoverButton2: '#EB5757'
+    hoverButton2: '#EB5757',
+    background: '#FFFFFF'
 }
