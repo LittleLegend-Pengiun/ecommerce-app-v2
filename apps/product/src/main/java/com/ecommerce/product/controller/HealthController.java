33c86a21e47a26c09b060c40bc2436f2c7b55878
Hoang Anh <hoanganhleboy@gmail.com>
2025-03-14 00:44:17 +0700

    Init

---
+++ apps/product/src/main/java/com/ecommerce/product/controller/HealthController.java
@@ -0,0 +1,33 @@
+package com.ecommerce.product.controller;
+
+import com.ecommerce.product.application.kafka.producer.TestProducer;
+import com.ecommerce.product.application.request.TestKafkaRequest;
+import com.ecommerce.product.application.response.GenericResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Slf4j
+@Validated
+@RequiredArgsConstructor
+public class HealthController {
+    private final TestProducer testProducer;
+
+    @GetMapping("/")
+    public ResponseEntity<GenericResponse> checkSystemHealth() {
+        return new ResponseEntity<>(new GenericResponse("This is Product microservice!"), HttpStatus.OK);
+    }
+
+    @PostMapping("/services/test-kafka")
+    public ResponseEntity<GenericResponse> publishKafkaMessage(@Valid @RequestBody TestKafkaRequest request) {
+        testProducer.sendMessage("test-topic", request.getMessage());
+        return new ResponseEntity<>(new GenericResponse("Message sent"), HttpStatus.OK);
+    }
+}
